4fb0010644141fdbd4c12d1ab691c3143f89bef8
Pratheeknk1205 <118595525+Pratheeknk1205@users.noreply.github.com>
2023-02-08 03:02:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-module "aft_backend" {
+module "packaging" {
+  source = "./s3"
+}
+  
+module "s3" {
   providers = {
     aws.primary_region   = aws.aft_management
     aws.secondary_region = aws.tf_backend_secondary_region
   }
-  source           = "./modules/aft-backend"
+  source           = "./s"
   primary_region   = var.ct_home_region
   secondary_region = var.tf_backend_secondary_region
 }
